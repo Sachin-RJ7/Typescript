1c9b406a34459c449e16595b438d120e052355f5
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-09-01 14:42:42 +0500

    started learning and some changes in the ts config file

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hey, I am starting leaning typescript");
+var myName = "Sachin";
+myName = "Rohit";
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log(sum(1, 3));
---
+++ index.ts
@@ -0,0 +1,9 @@
+console.log("Hey, I am starting leaning typescript")
+let myName:string = "Sachin"
+myName = "Rohit"
+
+function sum(num1:number, num2:number):number {
+    return num1 + num2;
+}
+
+console.log(sum(1, 3));
