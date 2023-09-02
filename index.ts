8fb1c31790ec0fad6bcf9a6e0fffc86d382ddf65
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-09-02 22:13:50 +0500

    done arrays, arrays method and objects in typescript

--- index.ts
+++ index.ts
@@ -7,3 +7,61 @@ function sum(num1:number, num2:number):number {
 }
 
 console.log(sum(1, 3));
+
+// How to write Arrays in typescript
+    // using square brackets
+    const numbers:number[] = [1, 2, 3, 4, 5, 6]
+
+    // using Array constructor
+    const num:number[] = new Array(1, 2, 3, 4, 5, 6);
+
+    // map method 
+    const doubleData:number[] = numbers.map((num:number) => num * 2)
+    console.log(doubleData);
+    // same as in filter and reduce method
+
+// how to write Objects in typescript
+    // when we have one object, then we can do type casting like this:
+    const person:{
+        userName: string;
+        age: number;
+        address:{
+            city: string;
+            state: string;
+        }
+    } = {
+        userName: "Rohit",
+        age: 27,
+        address: {
+            city: "Baddi",
+            state: "HP"
+        }
+    }
+
+    // but if we have more than one, we can use 'alias' of Typescript, which makes code more readable, follow the DRY principle...
+    type PersonType = {
+        userName: string;
+        age: number;
+        address:{
+            city: string;
+            state: string;
+        }
+    }
+
+    const person2:PersonType = {
+        userName: "Mohit",
+        age: 27,
+        address: {
+            city: "Baddi",
+            state: "HP"
+        }
+    }
+
+    const person3:PersonType = {
+        userName: "Purohit",
+        age: 27,
+        address: {
+            city: "Baddi",
+            state: "HP"
+        }
+    }
