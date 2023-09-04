f726c38caeb4b7f8b23bce904fc34fae9d4effb3
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-09-04 15:00:52 +0500

    done array, objects, type and enum

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,36 @@
 "use strict";
-console.log("Sachin");
-let userName = "Sachin";
+// Array
+let stringArr = ["Rohit", "karn", "Arjun"];
+let guitars = ["start", "les paul", 550];
+let mixedData = ["Mohit", 24, true];
+stringArr[0] = "Ashish";
+stringArr.push("hey");
+guitars[0] = 'bye';
+guitars.unshift('jim');
+let test = [];
+let bands = []; // Array of strings values
+bands.push('ha ha ha');
+// Tuple
+let myTuple = ["Tarun", 27, true];
+let mixed = ["johng", 21, false];
+mixed = myTuple; // both are equal
+// myTuple = mixed;   // not equal
+// enum
+var Grade;
+(function (Grade) {
+    Grade[Grade["A"] = 1] = "A";
+    Grade[Grade["B"] = 2] = "B";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["D"] = 4] = "D";
+    Grade[Grade["E"] = 5] = "E";
+})(Grade || (Grade = {}));
+const myObj1 = {
+    name: "Ajay",
+    roll_no: 1,
+    isActive: false,
+};
+const myObj2 = {
+    name: "Rahul",
+    roll_no: 2,
+    isActive: true,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,53 @@
-console.log("Sachin")
+// Array
+let stringArr = ["Rohit", "karn", "Arjun"]
+let guitars = ["start", "les paul", 550];
+let mixedData = ["Mohit", 24, true]
 
-let userName = "Sachin";
+stringArr[0] = "Ashish";
+stringArr.push("hey");
+
+guitars[0] = 'bye';
+guitars.unshift('jim')
+
+let test = []
+let bands:string[] = []  // Array of strings values
+bands.push('ha ha ha');
+
+// Tuple
+    let myTuple:[string, number, boolean] = ["Tarun", 27, true]
+
+    let mixed = ["johng", 21, false];
+
+    mixed = myTuple;  // both are equal
+
+ // myTuple = mixed;   // not equal
+
+
+// enum
+
+enum Grade {
+    A = 1, 
+    B, 
+    C, 
+    D, 
+    E 
+}
+
+// type
+type XYZ = {
+    name: string;
+    roll_no: number;
+    isActive: boolean;
+}
+
+const myObj1: XYZ ={
+    name: "Ajay",
+    roll_no: 1,
+    isActive: false,
+}
+
+const myObj2: XYZ ={
+    name: "Rahul",
+    roll_no: 2,
+    isActive: true,
+}
